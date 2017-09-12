8c6b1e7e01a16711facbf57a9024ebb9ceb02665
Paul Walsh <twentytwentyk@live.com>
2017-09-12 16:55:50 -0400

    first

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+exports.index =  function(req, res){
+  res.render('index', { title: 'Pauls Music Blog' });
+};
+
+exports.audio =  function(req, res){
+  res.render('audio', { title: 'Audio' });
+};
+
+exports.video =  function(req, res){
+  res.render('video', { title: 'Video' });
+};
+
+exports.blog =  function(req, res){
+  res.render('blog', { title: 'Blog' });
+};
